c54d9e4c7b3a2a8920eec171df7fa596c6229ebc
Plüß Martin <Kurunsh@yahoo.de>
2017-09-29 17:08:16 +0200

    Issue #3 Adding code for File interface

--- src/main/kurunsh/ventaja/model/out/File.java
+++ src/main/kurunsh/ventaja/model/out/File.java
@@ -4,5 +4,11 @@ import java.util.List;
 
 public interface File {
 
+  /**
+   * Returns the {@link List} of {@link Line}s.
+   *
+   * @return The {@link Line}s.
+   */
+  List<Line> getLines();
 
 }
